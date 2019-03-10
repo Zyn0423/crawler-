3062a40c3d1027c22fe32eaa7f8d5475579e5d7a
Zyn0423 <ainics@vip.qq.com>
2019-03-10 23:44:08 +0800

    '实现翻页功能'

--- Spiders_/tenxun/tenxun/settings.py
+++ Spiders_/tenxun/tenxun/settings.py
@@ -16,10 +16,10 @@ NEWSPIDER_MODULE = 'tenxun.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'tenxun (+http://www.yourdomain.com)'
+USER_AGENT = '"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36"'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- Spiders_/tenxun/tenxun/spiders/tencent.py
+++ Spiders_/tenxun/tenxun/spiders/tencent.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import time
 
 class TencentSpider(scrapy.Spider):
     name = 'tencent'
@@ -8,9 +8,19 @@ class TencentSpider(scrapy.Spider):
     start_urls = ['http://hr.tencent.com/position.php']
 
     def parse(self, response):
-        data=response.xpath('')
+        data=response.xpath("//*[@class='tablelist']//tr")[1:-1]
+        "职位类别	人数	地点	发布时间"
         for i in data:
             item={}
-            i.xpath('')
+            item['职位名称']=i.xpath("./td/a/text()").extract_first()
+            print(item)
+        next_=response.xpath("//*[@id='next']/@href").extract_first()
+        print(next_)
+
+        if next_ !='javascript:;':
+            time.sleep(2)
+            url_next='http://hr.tencent.com/'+ next_
+            yield scrapy.Request(url=url_next,callback=self.parse)
+
 
         pass
