1ca7d28f682f16ced5f16c7fcb48339863a198c3
Zyn0423 <ainics@vip.qq.com>
2019-03-19 21:02:19 +0800

    '构造爬虫、调度器'

--- Spiders_/scrapy_me/core/scheduler.py
+++ Spiders_/scrapy_me/core/scheduler.py
@@ -1,3 +1,33 @@
 # 调度器组件
 # 缓存请求对象(Request)，并为下载器提供请求对象，实现请求的调度
 # 对请求对象进行去重判断
+
+# coding=utf-8
+from queue import Queue
+# from six.moves.queue import Queue
+class Scheduler(object):
+    def __init__(self):
+        self.q=Queue()
+
+    def add_request(self,request):
+        """
+        添加url放入队列
+        :param request: url
+        :return:
+        """
+        self.q.put(request)
+
+    def get_request(self):
+        """
+        从队列里取出并返回
+        :return:
+        """
+        request=self.q.get()
+        return request
+
+    def _filter_request(self):
+        """
+        请求去重
+        :return:
+        """
+        pass
--- Spiders_/scrapy_me/core/spider.py
+++ Spiders_/scrapy_me/core/spider.py
@@ -1,4 +1,25 @@
+# coding=utf-8
+from scrapy_me.http.request import Request
+from scrapy_me.item import Item
 # 爬虫组件
 # 构建请求信息(初始的)，也就是生成请求对象(Request)
 # 解析响应对象，返回数据对象(Item)
 # 或者新的请求对象(Request)
+class Spider(object):
+    urls = 'http://www.baidu.com'
+
+    def start_requests(self):
+        """
+        调用request对象发送起始ＵＲＬ
+        :return:
+        """
+        return Request(url=self.urls)
+
+    def pares(self,response):
+
+        """
+        调用item对象
+        :param response: 响应参数
+        :return: 返回响应体
+        """
+        return Item(response.body)
