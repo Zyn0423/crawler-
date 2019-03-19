cb74817c7a34f7437467bbda7dd150966fcf89a8
Zyn0423 <ainics@vip.qq.com>
2019-03-19 21:59:00 +0800

    '构造下载器对象,并返回'

--- Spiders_/scrapy_me/core/downloader.py
+++ Spiders_/scrapy_me/core/downloader.py
@@ -1,3 +1,31 @@
+# coding=utf-8
 # 下载器组件
 # 根据请求对象(Request)，发起HTTP、HTTPS网络请求，拿到HTTP、HTTPS响应，构建响应对象(Response)
 # 并返回
+
+from scrapy_me.http.response import Response
+import requests
+
+class Downloader(object):
+    # 1. 根据请求对象，发起请求，获取响应
+    def down_pares(self,request):
+        if request.method.upper('GET'):
+            resp=requests.get(url=request.url,headers=request.headers)
+
+        elif request.method.upper('POST'):
+            resp=requests.post(url=request.url,headers=request.headers,data=request.data)
+
+        else:
+            raise Exception ('暂时不支持method其他请求方式')
+        """
+        # 响应地址
+        resp.url
+        # 响应状态码
+        resp.status_code
+        # 响应体
+        resp.body
+        # 响应头
+        resp.headers
+        """
+        # 2. 构建响应对象,并返回
+        return Response(url=resp.url,status_code=resp.status_code,body=resp.content,headers=resp.headers)
