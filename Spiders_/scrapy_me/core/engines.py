4d10bc9eb8d0b85ab8fdb7af3438e54f60b832d0
Zyn0423 <ainics@vip.qq.com>
2019-03-20 22:03:32 +0800

    爬虫多请求重新新建引擎、修改引擎：响应数大于等于请求数跳出循环

--- Spiders_/scrapy_me/core/engines.py
+++ Spiders_/scrapy_me/core/engines.py
@@ -9,7 +9,7 @@ from Spiders_.scrapy_me.middlewares.spider_middlewares import Spider_middlewares
 # 添加log
 from datetime import datetime
 from Spiders_.scrapy_me.utility.log import logger
-
+import time
 # 引擎组件
 # 负责驱动各大组件，通过调用各自对外提供的API接口，实现它们之间的交互和协作
 # 提供整个框架的启动入口
@@ -20,17 +20,28 @@ from Spiders_.scrapy_me.utility.log import logger
 取出的request对象交给下载的进行下载，返回response
 response交给爬虫模块进行解析，提取结果
 如果结果是request对象，重新交给调度器，如果结果是item对象，交给管道处理
+
+
+将代码拆分为两个方法，便于维护，提高代码可读性
+统计总共完成的响应数
+设置程序退出条件：当总响应数等于总请求数时，退出
+实现处理start_requests方法返回多个请求的功能
+实现处理parse解析函数返回多个对象的功能
 """
 
 
 class Engine(object):
-    def __init__(self):
+
+    def __init__(self, spider):  # 接收外部传入的爬虫对象
+        self.spider = spider  # 爬虫对象
         self.downloader = Downloader()
         self.pipeline = Pipeline()
-        self.spider = Spider()
         self.scheduler = Scheduler()
         self.downloaderMiddleware = DownloaderMiddleware()
         self.spider_middlewares = Spider_middlewares()
+
+        self.total_request_nums = 0
+        self.total_response_nums = 0
 
     def start(self):
         '''启动整个引擎'''
@@ -40,17 +51,32 @@ class Engine(object):
         stop = datetime.now()  # 结束时间
         logger.info("开始运行时间：%s" % stop)  # 使用日志记录结束运行时间
         logger.info("耗时：%.2f" % (stop - start).total_seconds())  # 使用日志记录运行耗时
+        logger.info("总的请求数量:{}".format(self.total_request_nums))
+        logger.info("总的响应数量:{}".format(self.total_response_nums))
 
-    def _start_engine(self):
+
+        # 此处新增
+
+    def _start_request(self):
         # 获取ＵＲＬ
         # 1.爬虫模块发出初始请求
-        start_request = self.spider.start_requests()
-        # 1.1利用爬虫中间件预处理请求对象
-        start_request = self.spider_middlewares.process_request(start_request)
-        # 2.把初始请求添加给调度器
-        self.scheduler.add_request(start_request)
+        for start_request in self.spider.start_requests():
+            # 1.1利用爬虫中间件预处理请求对象
+            start_request = self.spider_middlewares.process_request(start_request)
+            # 2.把初始请求添加给调度器
+            self.scheduler.add_request(start_request)
+
+            self.total_request_nums +=1
+
+
+
+    def _execute_request_response_item(self):
         # 3.从调度器获取请求对象，交给下载器发起请求，获取一个响应对象
         request = self.scheduler.get_request()
+        # 判断从调度器取出来后是不是空
+        if request is None:
+            return
+
         # 3.1利用下载器中间件预处理请求对象
         request = self.downloaderMiddleware.process_request(request)
         # 4.利用下载器发起请求
@@ -60,20 +86,33 @@ class Engine(object):
         # 4.2利用爬虫中间件预处理响应对象
         self.spider_middlewares.process_response(response)
         # 5．利用爬虫的解析响应的方式，处理响应，得到结果
-        resp = self.spider.pares(response)
-        # ６.判断结果对象
-        # 如果是请求对象，那么就在交给调度器
-        if isinstance(resp, Request):
-            # 6.1利用爬虫中间件预处理请求对象
-            resp = self.spider_middlewares.process_request(resp)
+        for resp in self.spider.pares(response):
+            # resp = self.spider.pares(response)
+            # ６.判断结果对象
+            # 如果是请求对象，那么就在交给调度器
+            if isinstance(resp, Request):
+                # 6.1利用爬虫中间件预处理请求对象
+                # 在解析函数得到request对象之后，使用process_request进行处理
+                resp = self.spider_middlewares.process_request(resp)
+                self.scheduler.add_request(resp)
+                self.total_request_nums += 1
+            # 7.如果不是，调用pipeline的process_item方法处理结果
+            else:
 
-            self.scheduler.add_request(resp)
-            # 否则，就交给管道处理
-        else:
-
-            self.pipeline.process_item(resp)
+                self.pipeline.process_item(resp)
+        self.total_response_nums += 1
 
 
-if __name__ == '__main__':
-    eng = Engine()
-    eng.start()
+
+    def _start_engine(self):
+
+      self._start_request()
+
+      while True:
+          time.sleep(0.001)
+          self._execute_request_response_item()
+          # 程序退出条件
+          if self.total_response_nums >= self.total_request_nums:
+              break
+
+
--- Spiders_/scrapy_me/core/spider.py
+++ Spiders_/scrapy_me/core/spider.py
@@ -6,20 +6,37 @@ from Spiders_.scrapy_me.item import Item
 # 解析响应对象，返回数据对象(Item)
 # 或者新的请求对象(Request)
 class Spider(object):
-    urls = 'http://www.baidu.com'
+    '''
+    1. 构建请求信息(初始的)，也就是生成请求对象(Request)
+    2. 解析响应对象，返回数据对象(Item)或者新的请求对象(Request)
+    '''
+    # urls = 'http://www.baidu.com'
+    start_urls = []
+
+    # def start_requests(self):
+    #     """
+    #     调用request对象发送起始ＵＲＬ
+    #     :return:
+    #     """
+    #     return Request(url=self.urls)
 
     def start_requests(self):
-        """
-        调用request对象发送起始ＵＲＬ
-        :return:
-        """
-        return Request(url=self.urls)
+        '''构建初始请求对象并返回'''
+        for url in self.start_urls:
+            yield Request(url)
 
+    # def pares(self,response):
+    #
+    #     """
+    #     调用item对象
+    #     :param response: 响应参数
+    #     :return: 返回响应体
+    #     """
+    #     return Item(response.body)
+    # 利用生成器方式实现，提高程序的资源消耗
     def pares(self,response):
-
-        """
-        调用item对象
-        :param response: 响应参数
-        :return: 返回响应体
-        """
-        return Item(response.body)
+        '''解析请求
+        并返回新的请求对象、或者数据对象
+        返回值应当是一个容器，如start_requests返回值方法一样，改为生成器即可
+        '''
+        yield Item(response.body)   # 返回item对象 改为生成器即可
