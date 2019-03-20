d2e713e4ba102d375a7799df290ab2f399aec2b7
Zyn0423 <ainics@vip.qq.com>
2019-03-20 19:33:40 +0800

    构造下载器中间件、爬虫中间件

---
+++ Spiders_/scrapy_me/middlewares/spider_middlewares.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+"""构造爬虫中间件"""
+class Spider_middlewares(object):
+
+    def process_request(self,request):
+        '''预处理请求对象'''
+        print("这是爬虫中间件：process_request方法")
+        return request
+
+
+
+    def process_response(self,response):
+        '''预处理响应对象'''
+        print("这是爬虫中间件：process_response方法")
+        return response
