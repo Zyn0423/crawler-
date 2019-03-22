cddb63e70f16894901e5a405afbe53e5e99939da
Zyn0423 <ainics@vip.qq.com>
2019-03-22 14:40:01 +0800

    添加baidu，parse方法

--- Spiders_/scrapy_me/core/test/baidu_spider.py
+++ Spiders_/scrapy_me/core/test/baidu_spider.py
@@ -1,10 +1,19 @@
 from Spiders_.scrapy_me.core.spider import Spider
+from Spiders_.scrapy_me.http.request import Request
 # 继承框架的爬虫基类
 class BaiduSpider(Spider):
     # 为爬虫命名
     name='baidu'
 
     # 设置初始请求url
-    start_urls = ['http://www.baidu.com']
+    # start_urls =[]
 
-
+    def start_requests(self):
+        base_url = 'http://www.baidu.com'
+        yield Request(base_url)
+    #     pass
+    #
+    def parse(self, response):
+        print(response.url)
+        yield {}
+        pass
